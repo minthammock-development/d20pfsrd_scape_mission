e5f522455cf2f1b7a59ec85be4e8114619ae7b52
Michael Mahoney <minthammockdevelopment@gmail.com>
2021-05-03 14:52:19 -0600

    scrape project complete. remaining preprocessing to be done in notebook.

--- d20pfsrd_scape/d20pfsrd_scape/settings.py
+++ d20pfsrd_scape/d20pfsrd_scape/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'd20pfsrd_scape.spiders'
 #USER_AGENT = 'd20pfsrd_scape (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- d20pfsrd_scape/d20pfsrd_scape/spiders/items_spider.py
+++ d20pfsrd_scape/d20pfsrd_scape/spiders/items_spider.py
@@ -8,30 +8,26 @@ class ItemSpider(scrapy.Spider):
   def start_requests(self):
       urls = [
           'https://www.d20pfsrd.com/magic-items/rings',
-          'https://www.d20pfsrd.com/magic-items/staves',
-          'https://www.d20pfsrd.com/magic-items/wands',
-          'https://www.d20pfsrd.com/magic-items/wondrous-items',
-          'https://www.d20pfsrd.com/magic-itemsartifacts',
-          'https://www.d20pfsrd.com/magic-items/intelligent-items',
-          'https://www.d20pfsrd.com/magic-items/cursed-items',
+          # 'https://www.d20pfsrd.com/magic-items/staves',
+          # 'https://www.d20pfsrd.com/magic-items/wands',
+          # 'https://www.d20pfsrd.com/magic-items/wondrous-items',
+          # 'https://www.d20pfsrd.com/magic-items/artifacts',
+          # 'https://www.d20pfsrd.com/magic-items/intelligent-items',
+          # 'https://www.d20pfsrd.com/magic-items/cursed-items',
       ]
       for url in urls:
           yield scrapy.Request(url=url, callback=self.parse)
 
   def parse(self, response):
       # page = response.url.split("/")[-2]
-      hrefs = response.css('li.page\ new\ parent a::attr(href)').getall()
-      yield from response.follow_all(hrefs, callback = self.chaseLinks)
+      for href in response.css('div.ogn-childpages ul.ogn-childpages li a::attr(href)').getall():
+        yield response.follow(href, callback = self.chaseLinks)
+        time.sleep(.1)
 
-  def chaseLinks(self, response):
-    yield 
-    {
-      'html' : response.body
+  def chaseLinks(self, res):
+    itemName = res.css('main h1::text').get()
+    itemInfo = res.css('div.page-center p::text').getall()
+    yield {
+      'name' : itemName,
+      'info' : itemInfo
     }
-    time.sleep(.3)
-
-
-  filename = f'quotes-{page}.json'
-  with open(filename, 'wb') as f:
-      f.write(response.body)
-  self.log(f'Saved file {filename}')
